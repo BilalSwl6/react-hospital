5bacc26aa177bf0a509b3fe491f3f44c99367dcd
Muhammad Bilal Saleem <71386745+BilalSwl6@users.noreply.github.com>
2025-06-28 18:18:57 +0000

    fix error

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -21,8 +21,6 @@ return Application::configure(basePath: dirname(__DIR__))
             HandleInertiaRequests::class,
             AddLinkHeadersForPreloadedAssets::class,
         ]);
-        $middleware->trustHosts(['*']);
-        $middleware->trustProxies(at: '*');
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
