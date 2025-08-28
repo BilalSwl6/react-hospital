596e181df35bc275098b721492d41fdcf49ddaac
mbilal2913 <mbilal2913@gmail.com>
2025-08-28 21:21:35 +0000

    fix error - name not showing in siedbar

--- resources/js/components/app-logo.tsx
+++ resources/js/components/app-logo.tsx
@@ -3,13 +3,13 @@ import { type SharedData } from '@/types';
 
 export default function AppLogo() {
     const { site_name, site_logo } = usePage<SharedData>().props.settings;
-    // const data = usePage<SharedData>().props;
-    // console.log(data)
+    const data = usePage<SharedData>().props;
+    console.log(data)
     return (
         <>
             <div className="dark:bg-white text-sidebar-primary-foreground flex aspect-square size-8 items-center justify-center rounded-md">
                 {/* <AppLogoIcon className="size-5 fill-current text-white dark:text-black" /> */}
-                <img src={site_logo ?? '/logo.png'} alt="Logo" className="size-7 fill-current text-white dark:text-black" />
+                <img src={site_logo ? `/storage/${site_logo}` : '/logo.png'} alt="Logo" className="size-7 fill-current text-white dark:text-black" />
             </div>
             <div className="ml-1 grid flex-1 text-left text-sm">
                 <span className="mb-0.5 truncate leading-none font-semibold" title={site_name}>{site_name}</span>
